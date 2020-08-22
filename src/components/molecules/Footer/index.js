2e68b54a7366eda372a871a10f666b6bdcf1dcae
aryan.verma <aryan.verma@navyuginfo.com>
2020-08-22 11:26:28 +0530

    final commit

---
+++ src/components/molecules/Footer/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './style.css';
+
+const Footer = (props) => {
+  return (
+    <footer className="Footer">
+      <section>
+        Designed and built with{' '}
+        <span className="Footer__Heart">&hearts;</span> by{' '}
+
+          Aryan Verma
+
+      </section>
+
+
+    </footer>
+  );
+};
+
+export default Footer;
